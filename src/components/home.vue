0b60a6dc708c309b64928afad9a9cb34ef5ca930
陈杰 <5353543+lvyier@user.noreply.gitee.com>
2020-06-07 17:14:58 +0800

    init project

---
+++ src/components/home.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>这是home页面</h1>
+</template>
+<script>
+export default {}
+</script>
+<style scope></style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import '../src/assets/styles/reset.css'
+import '../src/assets/styles/border.css'
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 new Vue({
   router,
   render: h => h(App)
