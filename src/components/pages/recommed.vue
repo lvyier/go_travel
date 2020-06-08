6ae968e03a0ab1d572be23f201fd3cc6ee32894f
陈杰 <5353543+lvyier@user.noreply.gitee.com>
2020-06-08 20:44:14 +0800

    updata

--- src/components/pages/recommed.vue
+++ src/components/pages/recommed.vue
@@ -1,44 +1,44 @@
 <template>
-    <div>
-        <div class="recommend_title">热销推荐</div>
+  <div>
+    <div class="recommend_title">热销推荐</div>
     <ul>
-        <li class="item" v-for=" item of recommedList" :key="item.id">
-            <img class="item_img" :src="item.URLImg" alt="">
-            <div class="item_info">
-                <p class="item_title">{{item.title}}</p>
-                <p class="item_desc">{{item.detail}}</p>
-                <button class="item_btn">查看详情</button>
-            </div>
-        </li>
+      <li class="item" v-for="item of recommedList" :key="item.id">
+        <img class="item_img" :src="item.URLImg" alt="" />
+        <div class="item_info">
+          <p class="item_title">{{ item.title }}</p>
+          <p class="item_desc">{{ item.detail }}</p>
+          <button class="item_btn">查看详情</button>
+        </div>
+      </li>
     </ul>
-    </div>
+  </div>
 </template>
 <script>
 export default {
-    data(){
-        return {
-            recommedList:[
-                {
-                    id:'001',
-                    URLImg:'https://imgs.qunarzz.com/p/tts7/2005/3d/c41d648b27c1c002.jpg_160x160_282e2e32.jpg',
-                    title:'畅游古镇',
-                    detail:'古北水镇一日游（50%空座率+分餐制+日游+夜游）'
-                },
-                {
-                    id:'002',
-                    URLImg:'https://imgs.qunarzz.com/p/tts4/1912/23/9052593c0e693402.jpg_160x160_23bb7c34.jpg',
-                    title:'故宫半日游',
-                    detail:'北京故宫+天安门广场+国家博物馆纯玩一日游'
-                },
-                {
-                    id:'003',
-                    URLImg:'https://imgs.qunarzz.com/p/tts0/1905/1b/acbaa67ca03c8502.png_160x160_53d17ced.png',
-                    title:'五环上门',
-                    detail:'北京故宫+八达岭长城1日游|故宫深度半日游'
-                }
-            ]
+  data() {
+    return {
+      recommedList: [
+        {
+          id: '001',
+          URLImg: 'https://imgs.qunarzz.com/p/tts7/2005/3d/c41d648b27c1c002.jpg_160x160_282e2e32.jpg',
+          title: '畅游古镇',
+          detail: '古北水镇一日游（50%空座率+分餐制+日游+夜游）'
+        },
+        {
+          id: '002',
+          URLImg: 'https://imgs.qunarzz.com/p/tts4/1912/23/9052593c0e693402.jpg_160x160_23bb7c34.jpg',
+          title: '故宫半日游',
+          detail: '北京故宫+天安门广场+国家博物馆纯玩一日游'
+        },
+        {
+          id: '003',
+          URLImg: 'https://imgs.qunarzz.com/p/tts0/1905/1b/acbaa67ca03c8502.png_160x160_53d17ced.png',
+          title: '五环上门',
+          detail: '北京故宫+八达岭长城1日游|故宫深度半日游'
         }
+      ]
     }
+  }
 }
 </script>
 <style lang="stylus" scoped>
@@ -61,7 +61,7 @@ export default {
             flex: 1;
             padding: .1rem;
             min-width: 0;
-            .item_title 
+            .item_title
                 line-height: .54rem;
                 font-size: .32rem;
             .item_desc
@@ -73,6 +73,4 @@ export default {
                 padding: 0 .1rem;
                 border-radius: .06rem;
                 margin-top: .2rem;
-
-
 </style>
--- src/components/pages/weekend.vue
+++ src/components/pages/weekend.vue
@@ -1,46 +1,46 @@
 <template>
-    <div>
-        <div class="recommend_title">周末去哪儿</div>
+  <div>
+    <div class="recommend_title">周末去哪儿</div>
     <ul>
-        <li class="item" v-for=" item of recommedList" :key="item.id">
-            <div class="item_img_wrapper">
-                <img class="item_img" :src="item.URLImg" alt="">
-            </div>
-            
-            <div class="item_info">
-                <p class="item_title">{{item.title}}</p>
-                <p class="item_desc">{{item.detail}}</p>
-            </div>
-        </li>
+      <li class="item" v-for="item of recommedList" :key="item.id">
+        <div class="item_img_wrapper">
+          <img class="item_img" :src="item.URLImg" alt="" />
+        </div>
+
+        <div class="item_info">
+          <p class="item_title">{{ item.title }}</p>
+          <p class="item_desc">{{ item.detail }}</p>
+        </div>
+      </li>
     </ul>
-    </div>
+  </div>
 </template>
 <script>
 export default {
-    data(){
-        return {
-            recommedList:[
-                {
-                    id:'001',
-                    URLImg:'https://imgs.qunarzz.com/sight/source/1505/68/de862f94e383a6.jpg_r_640x214_f9df927b.jpg',
-                    title:'福州打卡',
-                    detail:'大美福州，必游之处'
-                },
-                {
-                    id:'002',
-                    URLImg:'https://imgs.qunarzz.com/sight/source/1505/f6/82acfa5c7f472c.jpg_r_640x214_508da3cb.jpg',
-                    title:'熊孩子玩乐',
-                    detail:'与数千种海洋动物亲切对话，看人鲨共舞惊心动魄、梦幻美人鱼婀娜多姿、海狮幽默的表演'
-                },
-                {
-                    id:'003',
-                    URLImg:'https://imgs.qunarzz.com/sight/source/1509/b5/07bc81fc254ed6.jpg_r_640x214_8879c815.jpg',
-                    title:'土楼秘密',
-                    detail:'一座座神秘的建筑，一段段传奇的历史，在这里深刻体会历史留下的点点足迹，感受闽文化'
-                }
-            ]
+  data() {
+    return {
+      recommedList: [
+        {
+          id: '001',
+          URLImg: 'https://imgs.qunarzz.com/sight/source/1505/68/de862f94e383a6.jpg_r_640x214_f9df927b.jpg',
+          title: '福州打卡',
+          detail: '大美福州，必游之处'
+        },
+        {
+          id: '002',
+          URLImg: 'https://imgs.qunarzz.com/sight/source/1505/f6/82acfa5c7f472c.jpg_r_640x214_508da3cb.jpg',
+          title: '熊孩子玩乐',
+          detail: '与数千种海洋动物亲切对话，看人鲨共舞惊心动魄、梦幻美人鱼婀娜多姿、海狮幽默的表演'
+        },
+        {
+          id: '003',
+          URLImg: 'https://imgs.qunarzz.com/sight/source/1509/b5/07bc81fc254ed6.jpg_r_640x214_8879c815.jpg',
+          title: '土楼秘密',
+          detail: '一座座神秘的建筑，一段段传奇的历史，在这里深刻体会历史留下的点点足迹，感受闽文化'
         }
+      ]
     }
+  }
 }
 </script>
 <style lang="stylus" scoped>
@@ -58,7 +58,7 @@ export default {
                 width:14.4rem;
         .item_info
             padding: .1rem;
-            .item_title 
+            .item_title
                 line-height: .54rem;
                 font-size: .32rem;
             .item_desc
