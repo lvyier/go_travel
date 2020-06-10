b34a1937f259ad3e31cd2df2face37814894b6d5
陈杰 <5353543+lvyier@user.noreply.gitee.com>
2020-06-10 18:27:14 +0800

    add city css

--- src/components/city/city.vue
+++ src/components/city/city.vue
@@ -2,15 +2,21 @@
   <div>
     <header-city></header-city>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphate></city-alphate>
   </div>
 </template>
 <script>
 import HeaderCity from './components/header.vue'
 import CitySearch from './components/search.vue'
+import CityList from './components/list.vue'
+import CityAlphate from './components/Alphate.vue'
 export default {
   components: {
     HeaderCity,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphate
   }
 }
 </script>
---
+++ src/components/city/components/Alphate.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="list">
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl'
+    .list
+        display: flex;
+        position: fixed;
+        top: 6rem;
+        right: 0;
+        width: .4rem;
+        flex-direction: column;
+        justify-content: center;
+        .item
+            line-height: .44rem;
+            color: $bgColor;
+            text-align: center;
+</style>
